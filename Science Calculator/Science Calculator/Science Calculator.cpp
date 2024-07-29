13540483161990acce6af9f00ebb35b5ef9f6091
Mikołaj Mach <mach.mikolaj@wp.pl>
2024-07-29 21:42:11 +0200

    Imaginary part of more complex calculations

--- Science Calculator/Science Calculator/Science Calculator.cpp
+++ Science Calculator/Science Calculator/Science Calculator.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
-#include <algorithm> // For sort function
+#include <algorithm>
+#include <complex>
 using namespace std;
 
 class Function {
@@ -50,7 +51,7 @@ public:
         return result;
     }
 
-    long double sqrt_newton(long double x) {
+    long double sqrt(long double x) {
         if (x < 0) {
             cout << "Square root of a negative number is not defined in real numbers." << endl;
             return -1;
@@ -86,7 +87,7 @@ public:
         return result;
     }
 
-    long double abs_custom(long double x) {
+    long double absolutValue(long double x) {
         long double result = x < 0 ? -x : x;
         cout << "Absolute value of " << x << " = " << result << endl;
         return result;
@@ -124,13 +125,75 @@ public:
         cout << "Median of the provided numbers = " << result << endl;
         return result;
     }
+    
+    long double factorial(long double x) {
+
+        int result = 1;
+        for (int i = x; i > 0; i--) {
+            result *= x;
+            x--;
+        }
+        cout << "Factorial of provided number = " << result << endl;
+        return result;
+    }
+
+    long double exponential(long double x) {
+        const int max_iterations = 100;
+        const long double tolerance = 1e-14;
+        long double term = 1;
+        long double sum = 1;
+
+        for (int i = 1; i < max_iterations; ++i) {
+            term *= x / i;
+            sum += term;
+            if (abs(term) < tolerance) {
+                break;
+            }
+        }
+        cout << "Exponential of " << x << " = " << sum << endl;
+        return sum;
+    }
+
+    long double log10(long double x) {
+        const long double ln_10 = 2.30258509299404568402;
+        long double ln_x = log_newton(x);
+        long double result = ln_x / ln_10;
+        cout << "Base-10 logarithm of " << x << " = " << result << endl;
+        return result;
+    }
+
+    complex<long double> add_complex(complex<long double> number1, complex<long double> number2) {
+        complex<long double> result = number1 + number2;
+        cout << "(" << number1.real() << " + " << number1.imag() << "i) + ("
+            << number2.real() << " + " << number2.imag() << "i) = "
+            << result.real() << " + " << result.imag() << "i" << endl;
+        return result;
+    }
+
+    complex<long double> subtract_complex(complex<long double> number1, complex<long double> number2) {
+        complex<long double> result = number1 - number2;
+        cout << "(" << number1.real() << " + " << number1.imag() << "i) - ("
+            << number2.real() << " + " << number2.imag() << "i) = "
+            << result.real() << " + " << result.imag() << "i" << endl;
+        return result;
+    }
+
+    complex<long double> parse_complex(const string& input) {
+        long double real = 0, imag = 0;
+        char sign, i;
+        stringstream ss(input);
+        ss >> real >> sign >> imag >> i;
+        if (sign == '-') imag = -imag;
+        return complex<long double>(real, imag);
+    }
+    
 };
 
 int main() {
     Function Calc;
 
     cout << "Which operation do you want to perform? " << endl;
-    cout << "1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Power\n6. Sqrt\n7. Log\n8. Absolute value\n9. Mean\n10. Median" << endl;
+    cout << "1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Power\n6. Sqrt\n7. Log\n8. Absolute value\n9. Mean\n10. Median \n11. Factorial\n12. Exponential\n13. Logarithm10\n14. Add (Complex)\n15. Subtract (Complex)" << endl;
     int response;
     cin >> response;
 
@@ -185,7 +248,7 @@ int main() {
         long double number;
         cout << "Enter the number to find the square root: ";
         cin >> number;
-        Calc.sqrt_newton(number);
+        Calc.sqrt(number);
         break;
     }
     case 7: {
@@ -199,7 +262,7 @@ int main() {
         long double abs_number;
         cout << "Enter the number to find the absolute value: ";
         cin >> abs_number;
-        Calc.abs_custom(abs_number);
+        Calc.absolutValue(abs_number);
         break;
     }
     case 9: {
@@ -224,6 +287,49 @@ int main() {
         cin.clear(); 
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
         Calc.median(numbers);
+        break;
+    }
+    case 11: {
+        long double fact_number;
+        cout << "Enter the number to find the factorial value: ";
+        cin >> fact_number;
+        Calc.factorial(fact_number);
+        break;
+    }
+    case 12: {
+        long double exp_number;
+        cout << "Enter the number to find the exponential value: ";
+        cin >> exp_number;
+        Calc.exponential(exp_number);
+        break;
+    }
+    case 13: {
+        long double log10_number;
+        cout << "Enter the number to find the log10 value: ";
+        cin >> log10_number;
+        Calc.exponential(log10_number);
+        break;
+    }
+    case 14: {
+        string input1, input2;
+        cout << "Enter the first complex number (a+bi): ";
+        getline(cin, input1);
+        cout << "Enter the second complex number (a+bi): ";
+        getline(cin, input2);
+        complex<long double> number1 = Calc.parse_complex(input1);
+        complex<long double> number2 = Calc.parse_complex(input2);
+        Calc.add_complex(number1, number2);
+        break;
+    }
+    case 15: {
+        string input1, input2;
+        cout << "Enter the first complex number (a+bi): ";
+        getline(cin, input1);
+        cout << "Enter the second complex number (a+bi): ";
+        getline(cin, input2);
+        complex<long double> number1 = Calc.parse_complex(input1);
+        complex<long double> number2 = Calc.parse_complex(input2);
+        Calc.subtract_complex(number1, number2);
         break;
     }
     default:
